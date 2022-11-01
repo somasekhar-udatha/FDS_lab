7c5e7e56acf03b9a2aed7c8ca4b89974f00ce87a
Soma Sekhar Udatha <115133440+somasekhar-udatha@users.noreply.github.com>
2022-11-02 00:33:57 +0530

    Create array_operations_numpy.py

---
+++ array_operations_numpy.py
@@ -0,0 +1,7 @@
+from numpy import *
+arr1 = array([[1,2,3],[4,5,6]])
+arr2 = array([[7,8,9],[10,11,12]])
+print("adding 1 to every element in arr1 : ",arr1+1)
+print("subtracting 2 from every element in arr2 : ",arr2-2)
+print("sum of elements in arr1 : ",sum(arr1))
+print("sum of arrays : ",arr1+arr2)
